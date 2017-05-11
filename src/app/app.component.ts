d96f2f4bdf29eb2a827265568f26a315dcc1ea50
Mike Repec <mikerepec@gmail.com>
2017-05-11 12:55:01 +0200

    Feature Added (+) - Added Versioning to display on the app front. Finaly updated app version number in package.json as well.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-	selector: 'app-root',
-	template: `
-	<div class="container">
-	<router-outlet></router-outlet>
-	</div>
-	`,
-	styles: []
+  selector: 'app-root',
+  template: `
+    <div class="container">
+      <router-outlet></router-outlet>
+    </div>
+    <div class="version">Version {{ version }}</div>
+  `,
+  styles: [
+      `
+      .version {
+        position: absolute;
+        top: 5px;
+        left: 5px;
+      }
+    `
+  ]
 })
 export class AppComponent {
+  version: string;
+
+  constructor() {
+    this.version = require('../../package.json').version;
+  }
 }
